3e06c2500b66cf181a8448d4b13760efadc80ada
Poul Kjeldager Sørensen <pks@s-innovations.net>
2014-01-11 20:50:55 +0100

    First Commit, lets see what can build from this.

---
+++ S-Innovations.AzurePublisher.TheWCDCDemoPlugin/S-Innovations.AzurePublisher.TheWCDCDemoPlugin/TheDemoDeployment.cs
@@ -0,0 +1,30 @@
+﻿using SInnovations.WindowsAzure.Publisher.Plugins;
+using SInnovations.WindowsAzure.Publisher.Plugins.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SInnovations.AzurePublisher.TheWCDCDemoPlugin
+{
+    public class TheDemoDeployment : ICloudServiceDeploymentModel
+    {
+
+        [DeploymentSetting(SettingName = "Composite.WindowsAzure.WebRole.DeploymentName", Writable = true)]
+        [Display(Name = "Deployment Name", Order = 1)]
+        [Required]
+        public string DeploymentName { get; set; }
+
+        public Uri GetPackageUri(System.Xml.Linq.XDocument configuration, string size = "ExtraSmall")
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ManipulateConfiguration(System.Xml.Linq.XDocument configuration)
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
